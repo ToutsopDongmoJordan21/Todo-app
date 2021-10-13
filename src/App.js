948d3777e2be2e2ee757315162a5ac48dc5b94bf
ToutsopDongmoJordan21 <toutsopjordan1999@gmail.com>
2021-10-13 21:23:04 +0100

    display what we enter in the input ini the ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './components/Form';
 import TodoList from'./components/TodoList';
 import './App.css';
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
-        <h1>Ed's Todo List</h1>
+        <h1>Ed's Todo List </h1>
       </header>
-      <Form />
+      <Form inputText ={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
       <TodoList />
     </div>
   );
